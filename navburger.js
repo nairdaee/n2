ee5558c7e0e0f7c8577a42cf15e6cf31b1442d02
Adrian Etenyi <etadriano2@gmail.com>
2020-08-04 17:47:29 +0300

    add logic for the burger animation

---
+++ navburger.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle nav
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+        //The links' animations
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = ''
+            }else{                                              //Please feel free to change the delay to your preference
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+    
+    
+}
+
+navSlide();
